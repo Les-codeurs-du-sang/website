f8526ccaa4effa6848b8a8384ea2a54f800754b9
Antoine <78476578+AntoineLORY@users.noreply.github.com>
2022-03-08 15:47:37 +0100

    JetStream
    Authentification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,44 +1,42 @@
 <!DOCTYPE html>
-<html lang="en-US" dir="ltr">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>LaslesVPN | Landing &amp; Corporate Template</title>
 
-<!-- ===============================================-->
-    <!--    Document Title-->
-    <!-- ===============================================-->
-    <title>LaslesVPN | Landing &amp; Corporate Template</title>
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link href="{{ asset('assets/css/theme.css') }}" rel="stylesheet" />
 
 
-    <!-- ===============================================-->
-    <!--    Favicons-->
-    <!-- ===============================================-->
+        @livewireStyles
 
-    <!-- ===============================================-->
-    <!--    Stylesheets-->
-    <!-- ===============================================-->
-    <link href="{{ asset('assets/css/theme.css') }}" rel="stylesheet" />
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    @include('includes.navigation')
+    @yield('content')
+    @include('includes.footer')
+    <body class="font-sans antialiased">
+        <x-jet-banner />
 
-</head>
-@include('includes.navigation')
 
-@yield('content')
+        @stack('modals')
 
-@include('includes.footer')
-<body>
-
-<!-- ===============================================-->
-<!--    JavaScripts-->
-<!-- ===============================================-->
-<script src="vendors/@popperjs/popper.min.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="vendors/is/is.min.js"></script>
-<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-<script src="{{ asset('assets/js/theme.js') }}"></script>
-
-<link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
-</body>
-
+        @livewireScripts
+        <script src="vendors/@popperjs/popper.min.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="vendors/is/is.min.js"></script>
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+        <script src="{{ asset('assets/js/theme.js') }}"></script>
+        <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+    </body>
 </html>
