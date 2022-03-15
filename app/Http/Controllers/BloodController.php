ccc3c74c8e423369b954bfb7af3cb7a9c6f2c84b
Axel Vion <66166281+UnPotDeYaourt@users.noreply.github.com>
2022-03-15 15:14:05 +0100

    Middleware sur la page Create

--- app/Http/Controllers/BloodController.php
+++ app/Http/Controllers/BloodController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class BloodController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth')->only('create');
+    }
     public function index() {
         $questions = Question::all();
         return view('blood.home', compact('questions'));
