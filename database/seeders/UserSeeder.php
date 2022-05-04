f98c17755d6c6ed223e7b09aaef16c40611a02cf
Axel Vion <axel.vionyt@gmail.com>
2022-05-04 16:35:19 +0200

    Ajout user eleve

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -35,5 +35,16 @@ class UserSeeder extends Seeder
             'isAdmin' => 1,
             'promotion_id' => 1
         ]);
+
+        User::create([
+            'name' => 'eleve',
+            'lastname' => 'eleve',
+            'pseudo' => 'eleve',
+            'email' => 'eleve@eleve.fr',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'remember_token' => Str::random(10),
+            'isAdmin' => 0,
+            'promotion_id' => 1
+        ]);
     }
 }
